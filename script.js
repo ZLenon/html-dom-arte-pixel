cfe9b0918150746d41926ec2d9490fa950f83913
ZLenon <lenoninformatica@gmail.com>
2022-07-05 18:55:21 -0300

    Zero evolução

---
+++ script.js
@@ -0,0 +1,14 @@
+let color01 = document.getElementsByName('#black')[3];
+let color02 = document.getElementsByName('#red')[3];
+let color03 = document.getElementsByName('#green')[3];
+let color04 = document.getElementsByName('#blue')[3];
+let colorDefaut = document.getElementsByTagName('pixel-board');
+
+/*Iniciar com a cor preta selecionada*/
+function initialColor() {
+    onload.document.addEventListener.();
+    document.getElementById('colorDefaut')[] = setIten.innerHTML'color01';
+}
+console.log(initialColor);
+
+ononline="initialColor()"
