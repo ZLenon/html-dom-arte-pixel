18522928551410c2a859f11ac497ebc8c3ee2ddf
ZLenon <lenoninformatica@gmail.com>
2022-07-07 19:14:03 -0300

    Tentando

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-let color01 = document.getElementsByName('#black')[3];
-let color02 = document.getElementsByName('#red')[3];
-let color03 = document.getElementsByName('#green')[3];
-let color04 = document.getElementsByName('#blue')[3];
-let colorDefaut = document.getElementsByTagName('pixel-board');
+let color01 = document.getElementsById('black');
+let color02 = document.getElementsById('red');
+let color03 = document.getElementsById('green');
+let color04 = document.getElementsById('blue');
+let colorDefaut = document.getElementsByClassName('pixel');
 
 /*Iniciar com a cor preta selecionada*/
-function initialColor() {
-    onload.document.addEventListener.();
-    document.getElementById('colorDefaut')[] = setIten.innerHTML'color01';
+function initialColor(event) {
+    event.preventDefaut();
+    onload.document.addEventListener.("select", color01);
 }
 console.log(initialColor);
 
-ononline="initialColor()"
