fa0e08b1cc7db9f8692cf8e14f7dade59b12b459
ZLenon <lenoninformatica@gmail.com>
2022-07-09 21:39:41 -0300

    Começando a desenrolar o projeto

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
-let color01 = document.getElementsById('black');
-let color02 = document.getElementsById('red');
-let color03 = document.getElementsById('green');
-let color04 = document.getElementsById('blue');
-let colorDefaut = document.getElementsByClassName('pixel');
+const colorPalet = document.getElementById('color-palette'); //chamei a ja existente div principal com a CLASS REFERENTE que esta no html
+console.log(colorPalet.id); //so para mostrar no console log, lembrar do ponto e do ID
 
-/*Iniciar com a cor preta selecionada*/
-function initialColor(event) {
-    event.preventDefaut();
-    onload.document.addEventListener.("select", color01);
+for (let index = 0; index < 4; index++) {
+  //percorre 4 indices
+  const div = document.createElement('div'); //CRIEI a div que não existia
+  div.setAttribute('class', 'color'); // addicionei ATRIBUTOS a div que acabei de criar a class nome color
+  if (index === 0) {     // como o projeto quer que pegue o primeiro atributo
+    div.classList.add('selected'); //Adicionei o comando SELECTED a div ZERO definida pelo IF
+    div.style.backgroundColor = 'black'; //Adicionei a COR PRETA a DIV ZERO
+  } else if (index === 1) {
+    div.style.backgroundColor = 'red'; //Addicionei a COR VERMELHA A DIV UM
+  } else if (index === 2) {
+    div.style.backgroundColor = 'green'; //Addicionei a COR VERDE A DIVI DOIS
+  } else {
+    div.style.backgroundColor = 'blue'; // Addicionei a COR VERDE A DIVI RESTANTE, no caso a utima
+  }
+  colorPalet.appendChild(div); //Chamei a Const colorPalet, a dei todos esses filho a ela usando o appendChild
 }
-console.log(initialColor);
-
